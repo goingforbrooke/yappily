30016a750141872ded8a7d5c46f8056b9abc90fc
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2025-01-15 15:21:08 -0800

    Remove unnecessary auth methods

--- main.py
+++ main.py
@@ -22,88 +22,60 @@ We use tweepy (Python, or the Rust equivalent in the future) because it's necess
  
 API key and secret are also knowns as consumer key and consumer secret. source: https://docs.x.com/resources/fundamentals/authentication/oauth-1-0a/api-key-and-secret#api-key-and-secret
 """
-from tweepy import Client, OAuth2UserHandler
+from tweepy import Client
 
 
 def main():
-    client_id = get_client_id()
-    redirect_uri = 'https://goingforbrooke.com'
-    scopes = ['tweet.read', 'tweet.write', 'offline.access']
-    client_secret = get_client_secret()
-
-    oauth2_user_handler = OAuth2UserHandler(client_id=client_id,
-                                            redirect_uri=redirect_uri,
-                                            scope=scopes,
-                                            # Client Secret is only necessary if using a confidential client
-                                            client_secret=client_secret)
-    auth_url = oauth2_user_handler.get_authorization_url()
-    print(auth_url)
-
-    # todo: Use PIN authentication b/c it's CLI friendly.
-    redirected_url = input("paste the full redirect url here: ")
-
-    fetch_response = oauth2_user_handler.fetch_token(redirected_url)
-    print(fetch_response)
-
-    access_token = fetch_response['access_token']
-    print(access_token)
-
-    api_client = Client(bearer_token=get_bearer_token(),
-                        consumer_key=get_consumer_key(),
-                        consumer_secret=get_consumer_secret(),
-                        access_token=access_token,
-                        access_token_secret=get_access_token_secret())
-
-    api_client.create_tweet(text="Hello, world!")
+    tweet_text = "Hello, world!"
+    send_tweet(tweet_text)
 
     print("Done")
 
-"""GOAL
 
-client = tweepy.Client(consumer_key='REPLACE_ME',
-                       consumer_secret='REPLACE_ME',
-                       access_token='REPLACE_ME',
-                       access_token_secret='REPLACE_ME')
+"""Send a tweet from Twitter/X."""
+def send_tweet(tweet_text: str):
+    consumer_key = get_consumer_key()
+    consumer_secret = get_consumer_secret()
+    access_token = get_access_token()
+    access_token_secret = get_access_token_secret()
 
-# Replace the text with whatever you want to Tweet about
-response = client.create_tweet(text='hello world')
+    api_client = Client(consumer_key=get_consumer_key(),
+                        consumer_secret=get_consumer_secret(),
+                        access_token=get_access_token(),
+                        access_token_secret=get_access_token_secret())
 
-print(response)
+    api_client.create_tweet(text=tweet_text)
 
-"""
+    print(f'Sent tweet: {tweet_text}')
+    return True
 
-def get_bearer_token():
-    with open('bearer_token.txt', 'r') as infile:
-        bearer_token = infile.read()
-    return bearer_token
 
-def get_access_token_secret():
-    with open('access_token_secret.txt', 'r') as infile:
-        access_token_secret = infile.read()
-    return access_token_secret
-
-def get_consumer_secret():
-    with open('api_key_secret.txt', 'r') as infile:
-        consumer_secret = infile.read()
-    return consumer_secret
-
+"""Consumer key is known as "API Key" in "Projects and Apps."""
 def get_consumer_key():
     with open('api_key.txt', 'r') as infile:
         consumer_key = infile.read()
     return consumer_key
 
-"""Load the client secret from a file."""
-def get_client_secret():
-    with open('client_secret.txt', 'r') as infile:
-        client_secret = infile.read()
-    return client_secret
+
+"""Consumer secret is known as "API Key" in "Projects and Apps."""
+def get_consumer_secret():
+    with open('api_key_secret.txt', 'r') as infile:
+        consumer_secret = infile.read()
+    return consumer_secret
 
 
-"""Load the client ID from a file."""
-def get_client_id():
-    with open('client_id.txt', 'r') as infile:
-        client_id = infile.read()
-    return client_id
+"""Get the access token from a file."""
+def get_access_token():
+    with open('access_token.txt', 'r') as infile:
+        access_token = infile.read()
+    return access_token
+
+
+"""Get the access token secret from a file."""
+def get_access_token_secret():
+    with open('access_token_secret.txt', 'r') as infile:
+        access_token_secret = infile.read()
+    return access_token_secret
 
 
 if __name__ == "__main__":
