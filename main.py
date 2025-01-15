ea0297a20f03c65f961ff05c19f0797bb439877b
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2025-01-15 14:52:05 -0800

    Add more auth handler functions

--- main.py
+++ main.py
@@ -19,20 +19,92 @@ We use tweepy (Python, or the Rust equivalent in the future) because it's necess
 [Oauth2 Glossary](https://docs.x.com/resources/fundamentals/authentication/oauth-2-0/authorization-code#glossary)
 
 [source](https://docs.x.com/resources/fundamentals/authentication/oauth-2-0/application-only#app-only-authentication-and-oauth-2-0-bearer-token)
+ 
+API key and secret are also knowns as consumer key and consumer secret. source: https://docs.x.com/resources/fundamentals/authentication/oauth-1-0a/api-key-and-secret#api-key-and-secret
 """
-from tweepy import API, OAuth2BearerHandler
+from tweepy import Client, OAuth2UserHandler
 
 
 def main():
+    client_id = get_client_id()
+    redirect_uri = 'https://goingforbrooke.com'
+    scopes = ['tweet.read', 'tweet.write', 'offline.access']
+    client_secret = get_client_secret()
+
+    oauth2_user_handler = OAuth2UserHandler(client_id=client_id,
+                                            redirect_uri=redirect_uri,
+                                            scope=scopes,
+                                            # Client Secret is only necessary if using a confidential client
+                                            client_secret=client_secret)
+    auth_url = oauth2_user_handler.get_authorization_url()
+    print(auth_url)
+
+    # todo: Use PIN authentication b/c it's CLI friendly.
+    redirected_url = input("paste the full redirect url here: ")
+
+    fetch_response = oauth2_user_handler.fetch_token(redirected_url)
+    print(fetch_response)
+
+    access_token = fetch_response['access_token']
+    print(access_token)
+
+    api_client = Client(bearer_token=get_bearer_token(),
+                        consumer_key=get_consumer_key(),
+                        consumer_secret=get_consumer_secret(),
+                        access_token=access_token,
+                        access_token_secret=get_access_token_secret())
+
+    api_client.create_tweet(text="Hello, world!")
+
+    print("Done")
+
+"""GOAL
+
+client = tweepy.Client(consumer_key='REPLACE_ME',
+                       consumer_secret='REPLACE_ME',
+                       access_token='REPLACE_ME',
+                       access_token_secret='REPLACE_ME')
+
+# Replace the text with whatever you want to Tweet about
+response = client.create_tweet(text='hello world')
+
+print(response)
+
+"""
+
+def get_bearer_token():
     with open('bearer_token.txt', 'r') as infile:
         bearer_token = infile.read()
+    return bearer_token
 
-    auth = OAuth2BearerHandler(bearer_token)
-    api = API(auth)
+def get_access_token_secret():
+    with open('access_token_secret.txt', 'r') as infile:
+        access_token_secret = infile.read()
+    return access_token_secret
 
-    api.update_status("Hello, world!")
-    
-    print("Done")
+def get_consumer_secret():
+    with open('api_key_secret.txt', 'r') as infile:
+        consumer_secret = infile.read()
+    return consumer_secret
+
+def get_consumer_key():
+    with open('api_key.txt', 'r') as infile:
+        consumer_key = infile.read()
+    return consumer_key
+
+"""Load the client secret from a file."""
+def get_client_secret():
+    with open('client_secret.txt', 'r') as infile:
+        client_secret = infile.read()
+    return client_secret
+
+
+"""Load the client ID from a file."""
+def get_client_id():
+    with open('client_id.txt', 'r') as infile:
+        client_id = infile.read()
+    return client_id
+
 
 if __name__ == "__main__":
     main()
